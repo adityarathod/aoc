0975b6bf9e94e8eee2b67062c904fce785487c7c
Aditya Rathod <15635979+adityarathod@users.noreply.github.com>
2020-12-02 14:12:19 -0800

    Add solution template

---
+++ 2020/template.py
@@ -0,0 +1,17 @@
+
+class Solution:
+    def __init__(self, input_file='2020/dayN.txt'):
+        with open(input_file, 'r') as f:
+            self.input = f.read().split('\n')
+
+    def part_one(self):
+        return None
+
+    def part_two(self):
+        return None
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    print('Part 1: ', solution.part_one())
+    print('Part 2: ', solution.part_two())
